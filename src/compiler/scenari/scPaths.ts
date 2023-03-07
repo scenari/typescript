45178451af55b199473c0df393f1aeee16a4d331
dar <dar@kelis.fr>
2023-03-07 10:53:53 +0100

    Cleanup

--- src/compiler/scenari/scPaths.ts
+++ src/compiler/scenari/scPaths.ts
@@ -3,24 +3,21 @@ import * as fs from "fs";
 export const SC_WUI_SCHEME = "sc+wui:";
 
 let searchDir = __dirname;
-let wuiBootstrapDir: string | null = null;
+let wuiRootDir: string | null = null;
 
-while (!wuiBootstrapDir) {
+while (!wuiRootDir) {
     const testDir = path.resolve(searchDir, "Wui_Bootstrap");
-    if (fs.existsSync(testDir)) wuiBootstrapDir = testDir;
+    if (fs.existsSync(testDir)) wuiRootDir = testDir;
     const newSearchDir = path.dirname(searchDir);
     if (newSearchDir == searchDir) break;
     searchDir = newSearchDir;
 }
-console.log(wuiBootstrapDir);
-
-
 
 export function isScPath(path: string) {
     return path.startsWith(SC_WUI_SCHEME);
 }
 
 export function resolveScPath(path: string) {
-    if (wuiBootstrapDir && path.startsWith(SC_WUI_SCHEME)) return wuiBootstrapDir + "/" + path.substring(SC_WUI_SCHEME.length);
+    if (wuiRootDir && path.startsWith(SC_WUI_SCHEME)) return wuiRootDir + "/" + path.substring(SC_WUI_SCHEME.length);
     return path;
 }
